80eeb94cf31a4afc825046a467db7ed186181805
Christopher Dresel <christopher.dresel@spatial-focus.net>
2016-03-28 21:21:10 +0200

    Added sample code and test project

---
+++ SampleProject.Test/AdditionalizerTest.cs
@@ -0,0 +1,19 @@
+﻿namespace SampleProject.Test
+{
+	using System.Diagnostics;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class AdditionalizerTest
+	{
+		[TestMethod]
+		public void Add_AAndBAreZero_ReturnsZero()
+		{
+			Additionalizer additionalizer = new Additionalizer();
+
+			int result = additionalizer.Add(0, 0);
+
+			Debug.Assert(result == 0);
+		}
+	}
+}
---
+++ SampleProject/Additionalizer.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleProject
+{
+	public class Additionalizer
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public int Substract(int a, int b)
+		{
+			return a - b;
+		}
+	}
+}
