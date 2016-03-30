7fd14047145614208e786b8d4ca51df98fee3f3b
Christopher Dresel <christopher.dresel@spatial-focus.net>
2016-03-30 13:48:12 +0200

    Include StyleCop and FxCop

---
+++ SampleProject/Disposer.cs
@@ -0,0 +1,14 @@
+﻿namespace SampleProject
+{
+	using System.IO;
+
+	public class Disposer
+	{
+		FileStream newFile;
+
+		public void NoDisposeMethod()
+		{
+			this.newFile = new FileStream(@"c:\temp.txt", FileMode.Open);
+		}
+	}
+}
